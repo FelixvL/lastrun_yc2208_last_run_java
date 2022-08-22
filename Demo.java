674e00a4426f6157becaf21744c95c1d7793b0dd
FelixvL <digitaleservice@gmail.com>
2022-08-22 11:11:54 +0200

    MC

--- Demo.java
+++ Demo.java
@@ -1,17 +1,27 @@
-class Demo{   // MB
+class Demo{   // MC
 	public static void main(String[] args){
 		System.out.println("a");
-		B b = new B();
+		R b = new R();
 		b.ben();
+
 	}	
 }
 
 
 class B{
 	int b;
-	void ben(){
+	void ben() throws Exception{
 		System.out.println("Ben toch");
-		throw new Exception();
+		throw new Exception();	
+	}
+}
+
+class R extends B{
+	int b = 30;
+	void ben(){
+		System.out.println("wtf");
 	}
 
 }
+
+
