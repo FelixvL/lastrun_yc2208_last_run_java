9b1d342c40e35dbb5cc29a4578846cc7f2140215
FelixvL <digitaleservice@gmail.com>
2022-08-22 10:53:36 +0200

    MA

--- Demo.java
+++ Demo.java
@@ -1,26 +1,17 @@
-class Demo{   // QL
+class Demo{   // MA
 	public static void main(String[] args){
-		System.out.println("Hij doet het");
-		Kleding jas = new Jas();
-		System.out.println(jas.knoop);
-		jas.knopen(5);
-
+		System.out.println("a");
+		B b = new B();
+		b.ben();
 	}	
 }
 
 
-class Jas extends Kleding{
-	int knoop = 55;
-	int prijs = 80;
-	int knopen(){
-		System.out.println("in knopen"+knoop);
-		return 17;
+class B{
+	int b;
+	void ben(){
+		System.out.println("Ben toch");
+		throw new RuntimeException();
 	}
-}
-class Kleding{
-	int prijs;
-	int knoop = 70;
-	void knopen(int a){
-		System.out.println("in knopen kleding"+knoop);
-	}
+
 }
