7ff2baac8eaa6dabd405ab7528617e63644a181e
FelixvL <digitaleservice@gmail.com>
2022-08-22 10:07:15 +0200

    QB

---
+++ Demo.java
@@ -0,0 +1,19 @@
+class Demo{   // QB
+	public static void main(String[] args){
+		System.out.println("Hij doet het");
+		Jas jas = new Jas();
+		Jas jas2 = new Jas();
+	}	
+}
+
+
+class Jas{
+	static int knoop = knopen();
+	static int knopen(){
+		System.out.println("in knopen");
+		return 17;
+	}
+	Jas(){
+		System.out.println("in Jas");
+	}
+}
