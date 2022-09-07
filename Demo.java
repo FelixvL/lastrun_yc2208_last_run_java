5708bc48b5244d8f52a3561047eca8771fffa77f
FelixvL <digitaleservice@gmail.com>
2022-09-07 16:28:45 +0200

    UQ

--- Demo.java
+++ Demo.java
@@ -1,11 +1,12 @@
-class Demo{   // UP
+class Demo{   // UQ
 	public static void main(String[] args){
 		Fiets f = new Fiets();
-		Fiets f2 = new Fiets();
+		Fiets f2 = f;
 		lopen(f);
-		System.out.println("einde");
+		System.out.println("einde" + f2.a);
 	}
-	static void lopen(Object x){
+	static void lopen(Fiets x){
+		x.a = 25;
 		System.out.println("we zijn hier");
 	}
 }
